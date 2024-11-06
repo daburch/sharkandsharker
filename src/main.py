5ed500f3103c458e70d6b42815d13f8eafce1845
David Burch <david.allen.burch@gmail.com>
2024-11-05 20:11:56 -0500

    add keypress listener to stop execution

--- src/main.py
+++ src/main.py
@@ -1,10 +1,43 @@
 import argparse
 import logging
+import threading
+import time
+import keyboard
 
 from shark.shark import Shark, SharkConfig
 from config import MONITORED_IPS, LOCAL_DEFAULT_INTERFACE
 
 logger = logging.getLogger(__name__)
+
+
+def inspect(shark: Shark):
+    try:
+        t = threading.Thread(target=shark.packet_monitor.begin_monitoring)
+
+        t.start()
+
+        # infinite loop to print out the results of the watcher thread marketplace responses
+        l = len(shark.packet_monitor.responses)
+        while True:
+            if shark.is_stopped():
+                break
+
+            # print a simplified version of the most recent response if there is a new one
+            if l != len(shark.packet_monitor.responses):
+                for item in shark.packet_monitor.responses[-1].items:
+                    logger.info(f"{item.name}: {item.price}")
+
+                l = len(shark.packet_monitor.responses)
+
+            time.sleep(1)
+    finally:
+        t.join()
+
+
+def listen_for_keypress(shark: Shark):
+    keyboard.wait("`")
+    logger.info("Keypress detected. Stopping all threads.")
+    shark.end_monitoring()
 
 
 def main():
@@ -38,7 +71,17 @@ def main():
     if args.mode == "inspect":
         # Inspect Mode creates a packet watcher but doesn't automatically scan the marketplace; allowing manual scans.
         shark = Shark(SharkConfig(interface=LOCAL_DEFAULT_INTERFACE, ips=MONITORED_IPS))
-        shark.begin_monitoring()
+        packet_monitor_thread = threading.Thread(target=inspect, args=(shark,))
+        keypress_listener_thread = threading.Thread(
+            target=listen_for_keypress, args=(shark,)
+        )
+
+        packet_monitor_thread.start()
+        keypress_listener_thread.start()
+
+        packet_monitor_thread.join()
+        keypress_listener_thread.join()
+
     elif args.mode == "gather":
         # Gather Mode creates a packet watcher and automatically scans the marketplace to collect large training datasets.
         logger.info("TODO: Implement gather mode")
